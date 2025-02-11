c88613dd5ff6da8f11a59f3f73d2d830aef72854
Ethan Katsiroubas <136759655+EthanKatsi@users.noreply.github.com>
2025-02-11 17:57:09 -0500

    Finished Task I in Part 2 of Assignment #1

---
+++ group20_a1/t01.py
@@ -0,0 +1,206 @@
+import random
+import heapq
+import numpy as np
+import pandas as pd
+
+
+class Puzzle:
+    # Initializes the board
+    def __init__(self, board, parent = None, move = "", depth = 0, cost = 0):
+        self.board = tuple(board)  # Stores the tiles in the board as a tuple that can't be changed
+        self.parent = parent  # previous state so it can back track
+        self.move = move  # moves taken to reach this state
+        self.depth = depth  # num of moves made (g(n))
+        self.cost = cost  # cost = g(n) + h(n), g(n) is the num of moves made, h(n) is the estimated cost of the goal (heuristic value)
+        self.zero_index = board.index(0)  # Stores the position of the blank space in the puzzle
+
+    # Compares 2 puzzle objects to insert them into a prioritiy queue to ensure lower cost tiles are first
+    def __lt__(self, other):
+        return self.cost < other.cost
+        
+    # Compares the current board with the other board to prevent checking the same board
+    def __eq__(self, other):
+        return self.board == other.board
+    
+    # Gets the new tile arrangements on the board (neighbors) by moving the blank space 
+    def get_neighbors(self):
+        moves = []
+        row, column = divmod(self.zero_index, 3)  # Gets the row/column position of the current blank space from the current position of blank space to 3
+        # Dictionary of the directions of the board
+        directions = {
+            "Up": (-1, 0),
+            "Down": (1, 0),
+            "Left": (0, -1),
+            "Right": (0, 1)
+        }
+        
+        # Iterates over the directions dictionary
+        for move, (r, c) in directions.items():
+            new_row, new_column = row + r, column + c
+
+            if 0 <= new_row < 3 and 0 <= new_column < 3:
+                new_index_board = new_row * 3 + new_column  # Converts 2D row from the board into a 1D index
+                new_board = list(self.board)
+                # Swaps blank tile (zero_index) and new position tiles (new_index_board)
+                new_board[self.zero_index], new_board[new_index_board] = new_board[new_index_board], new_board[self.zero_index]
+                moves.append(Puzzle(new_board, self, move, self.depth + 1))  # Creates a new puzzle state by moving the blank tile and appends it to the list of possible moves
+        
+        return moves
+
+
+# h1: Misplaced tiles heuristic
+def misplaced_tiles(puzzle, goal):
+    count = 0 
+    for i in range(9):  # Iterates through all the tiles on board
+        if puzzle.board[i] != 0 and puzzle.board[i] != goal[i]:
+            count += 1
+    return count
+
+# h2: Manhattan distance heuristic
+def manhattan_distance(puzzle, goal_positions):  # Iterates through all tiles in board
+    total_distance = 0
+    for i in range(len(puzzle.board)):
+        tile = puzzle.board[i]  # Tile # at index i
+        if tile != 0:  # ignores the blank tile
+            row_distance = abs(i // 3 - goal_positions[tile][0])  # difference between the rows
+            column_distance = abs(i % 3 - goal_positions[tile][1])  # differnece btween columns
+            total_distance += row_distance + column_distance  # Adds the total distance between rows/columns to 
+
+    return total_distance
+
+# h3: Linear conflict heuristic (manhattan distance + conflicts)
+def linear_conflict(puzzle, goal_positions):
+    manhattan = manhattan_distance(puzzle, goal_positions)
+    conflicts = 0
+
+    for row in range(3):
+        row_tiles = []  # Tiles in current row
+        for column in range(3):
+            tile = puzzle.board[row * 3 + column]  # Get tile at the row/column
+            if tile in goal_positions:  # Checks if the tile is in the goal state
+                row_tiles.append(tile)
+
+        row_tiles_goal = []  # Tiles that should be in this row (their goal)
+        for tile in row_tiles:
+            if goal_positions[tile][0] == row:  # Checks if the tile is in this row
+                row_tiles_goal.append(tile)
+
+        for i in range(len(row_tiles_goal)):
+            tile_1st = row_tiles_goal[i]
+            for j in range(i + 1, len(row_tiles_goal)):
+                tile_2nd = row_tiles_goal[j]
+                if goal_positions[tile_1st][1] > goal_positions[tile_2nd][1]:  # Checks if the tile is misplaced
+                    # Conflicts are when 2 tiles are in their correct row/column but out of order in thier goal positions
+                    conflicts += 2
+
+    for column in range(3):
+         column_tiles = []  # Tiles in current column
+         for row in range(3):
+            tile = puzzle.board[row * 3 + column]
+            if tile in goal_positions:
+                column_tiles.append(tile)
+        
+         column_tiles_goal = []  # Tiles that should be in this column (their goal)
+         for tile in column_tiles:
+             if goal_positions[tile][1] == column:  # Checks if the tile is in this column
+                 column_tiles_goal.append(tile)
+
+         for i in range(len(column_tiles_goal)):
+            tile_1st = column_tiles_goal[i]
+            for j in range(i + 1, len(column_tiles_goal)):
+                tile_2nd = column_tiles_goal[j]
+                if goal_positions[tile_1st][0] > goal_positions[tile_2nd][0]:
+                    conflicts += 2
+
+    return manhattan + conflicts
+
+
+# Implements the A* search algorithm
+def a_star_search(start, goal, heuristic):
+    goal_positions = {}  # Stores correct positoins of each tile in goal state
+    
+    for i in range(len(goal)):
+        tile = goal[i]
+        if tile != 0:
+            goal_positions[tile] = (i // 3, i % 3)  # Stores the correct row/column positions of each tile
+
+    open_set = []  # Prio queue to store only the nodes to be explored
+    closed_set = set()  # Set to store the nodes that are already visitied to prevent nodes being explored more than once
+    heapq.heappush(open_set, (0, start))  # Pushes the empty tile (0) into the prio queue
+    nodes_expanded = 0
+
+    while open_set:  # Searches all nodes until there are no explored nodes left (open_set is empty)
+        _, current = heapq.heappop(open_set)  # Gets the node w the lowest cost
+        nodes_expanded += 1
+
+        if current.board == goal:  # If current tile matches the goal board then return the current depth and the # of nodes expanded
+            return current.depth, nodes_expanded
+        closed_set.add(current.board)  # Stores current tile in the closed set so it can mark it as a visited tile
+
+        # Iterates through all the neighboring tiles
+        for neighbor in current.get_neighbors():
+            if neighbor.board in closed_set:  # If tile is visited already it is skipped
+                continue
+            if heuristic == misplaced_tiles:
+                neighbor.cost = neighbor.depth + heuristic(neighbor, goal)
+            else:
+                neighbor.cost = neighbor.depth + heuristic(neighbor, goal_positions)
+            heapq.heappush(open_set, (neighbor.cost, neighbor))  # Adds new tile to prio queue to be explored
+
+    return None, None  # Return none if no solution is found in board
+
+
+# Checks if an 8 puzzle is solvable
+def is_solvable(board):
+    # counting the number of inversions
+    # Inversions are when a higher numbered tile on the board appears before the smaller one
+    count_inversions = 0
+    
+    for i in range(len(board)):
+        for j in range(i + 1, len(board)):
+            if board[i] and board[j] and board[i] > board[j]:  # If the title number is 0, it is ignored because it's a blank space
+                count_inversions += 1
+                    
+    return count_inversions % 2 == 0  # If the number of inversions is even, the puzzle is solvable
+
+# Generates 100 random puzzles
+def generate_solvable_puzzles(n = 100):
+    # Shuffles the titles until a solvable puzzle is found
+    puzzles = []  # All 100 randomly generated solvable puzzles
+    for _ in range(n):
+        while True:
+            tiles = list(range(9))  # 9 tiles from 0 to 8 (the board)
+            random.shuffle(tiles)
+            if is_solvable(tiles):  # Checks if the shuffled board is solvable, if it is then it gets added to the list of all 100 puzzles
+                puzzles.append(tiles)
+                break
+    return puzzles
+
+
+# Runs the A* algorithm and prints the data
+def run():
+    goal_state = (0, 1, 2, 3, 4, 5, 6, 7, 8)
+    puzzles = generate_solvable_puzzles(100)
+    
+    results = []
+    for heuristic in [misplaced_tiles, manhattan_distance, linear_conflict]:
+        steps, nodes = [], []
+        for puzzle in puzzles:
+            step_count, nodes_exp = a_star_search(Puzzle(puzzle), goal_state, heuristic)
+            
+            if step_count is not None:
+                steps.append(step_count)
+                nodes.append(nodes_exp)
+        
+        results.append({
+            "Puzzle Type": "8-puzzle",
+            "Heuristic": heuristic.__name__,
+            "Average Steps to Solution": np.mean(steps),
+            "Average Nodes Expanded": np.mean(nodes)
+        })
+    
+    df = pd.DataFrame(results)
+    
+    print(df.to_string(index = False))
+
+run()
